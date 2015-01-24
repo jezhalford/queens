d20ab6e454fd69289af8f77139978f7fd16a71c2
Jez Halford <jezhalford@gmail.com>
2015-01-24 14:01:26 +0000

    Made it work

--- queens.php
+++ queens.php
@@ -3,12 +3,15 @@
 
 
 
-$board = [][];
+$board = [];
 
 for($x=0; $x < 8; $x++) {
     for($y=0; $y < 8; $y++) {
-
+        $board[$x][$y] = null;
     }
 }
 
-function($board, $propsedX, $proposedY);
+
+var_dump($board);
+
+//function($board, $propsedX, $proposedY);
