81b9fed1a152f8527e727906884e9dc46da59510
Gerard Martin <gerjomarty@anientity.co.uk>
2015-01-24 14:28:08 +0000

    SORRY JEZ

--- queens.rb
+++ queens.rb
@@ -1,2 +1,28 @@
-board = [Array.new(8)] * 8
+require 'pp'
+require 'ostruct'
 
+QUEEN = "\u265B-"
+
+class Placement
+  def covered?
+    does_contain || 
+  end
+end
+
+def initial_board
+  ([Array.new(8)] * 8).collect {|row| row.collect { OpenStruct.new } }
+end
+
+def can_place_queen? board, x, y
+  (1..8).each do |i|
+    return !board[x][i].covered?
+    return !board[i][y].covered?
+    if x-i >= 0 && x-i < 8 && y+i >= 0 && y+i < 8 
+      return !board[x-i][y+i].covered?
+    end
+    if x+i >= 0 && x+i < 8 && y-i >= 0 && y-i < 8 
+      return !board[x+i][y-i].covered?
+    end
+  end
+  true
+end
